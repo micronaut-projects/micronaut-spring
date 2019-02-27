d59f9eef7536317b3128d84bff1a6efc1154d8fc
graemerocher <graeme.rocher@gmail.com>
2019-02-27 12:01:12 +0100

    Fix faulty test

--- spring-boot-annotation/src/test/groovy/io/micronaut/spring/boot/annotation/condition/ConditionalOnSingleCandidateComponent2.java
+++ spring-boot-annotation/src/test/groovy/io/micronaut/spring/boot/annotation/condition/ConditionalOnSingleCandidateComponent2.java
@@ -1,10 +1,11 @@
 package io.micronaut.spring.boot.annotation.condition;
 
 import io.micronaut.core.convert.TypeConverter;
+import io.micronaut.http.bind.binders.RequestArgumentBinder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnSingleCandidate(TypeConverter.class)
+@ConditionalOnSingleCandidate(RequestArgumentBinder.class)
 public class ConditionalOnSingleCandidateComponent2 {
 }
