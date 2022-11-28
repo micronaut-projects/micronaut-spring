7c4223d1820fb6ede274727fc347087bda9f1096
Graeme Rocher <graeme.rocher@gmail.com>
2022-11-28 09:43:58 +0100

    feat: Implement exporting spring beans to Micronaut (#357)
    * Allow defining extra beans via annotation

--- spring-boot-starter/src/main/java/io/micronaut/spring/boot/starter/EnableMicronaut.java
+++ spring-boot-starter/src/main/java/io/micronaut/spring/boot/starter/EnableMicronaut.java
@@ -37,4 +37,31 @@ public @interface EnableMicronaut {
      * @return The bean filter
      */
     Class<? extends MicronautBeanFilter> filter() default MicronautBeanFilter.class;
+
+    /**
+     * Defines one or more types that represent Spring beans that should be exposed to the Micronaut context.
+     * <p>Note care should be taken that circular dependencies are not introduced between the Spring context and the Micronaut context</p>
+     * @return The exposed bean.
+     */
+    ExposedBean[] exposeToMicronaut() default  {};
+
+    /**
+     * Used to allow to expose beans from the Spring to the Micronaut context.
+     */
+    @interface ExposedBean {
+        /**
+         * @return The bean type.
+         */
+        Class<?> beanType();
+
+        /**
+         * @return The name of the bean in the Spring context otherwise all beans of the type are searched.
+         */
+        String name() default "";
+
+        /**
+         * @return The qualifier to use for the Micronaut bean that is registered, defaults to the Spring Bean name.
+         */
+        String qualifier() default "";
+    }
 }
