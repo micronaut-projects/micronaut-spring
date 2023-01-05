726697cec4f9ae7ca13cc0faaf5f848ab33e4632
Graeme Rocher <graeme.rocher@gmail.com>
2023-01-05 16:08:34 +0100

    fix: Don't expose data source by default as it can conflict (#384)

--- spring-boot-starter/src/test/java/io/micronaut/spring/boot/customautoconfigure/AutoConfigureMicronautTest.java
+++ spring-boot-starter/src/test/java/io/micronaut/spring/boot/customautoconfigure/AutoConfigureMicronautTest.java
@@ -2,12 +2,16 @@ package io.micronaut.spring.boot.customautoconfigure;
 
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.codec.MediaTypeCodecRegistry;
 import io.micronaut.inject.BeanDefinition;
 import io.micronaut.spring.boot.starter.EnableMicronaut;
 import io.micronaut.spring.boot.starter.MicronautBeanFilter;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
@@ -20,6 +24,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class AutoConfigureMicronautTest {
+    @Autowired
+    ReceiveDatasource datasource;
 
     @Autowired
     ApplicationContext context;
@@ -38,7 +44,9 @@ public class AutoConfigureMicronautTest {
 
 
 @SpringBootApplication
-@EnableMicronaut(filter = MyFilter.class)
+@EnableMicronaut(filter = MyFilter.class, exposeToMicronaut =  @EnableMicronaut.ExposedBean(
+    beanType = DataSource.class
+))
 class Application {
 
 }
@@ -48,4 +56,9 @@ class MyFilter implements MicronautBeanFilter {
     public boolean excludes(@NonNull BeanDefinition<?> definition) {
         return MediaTypeCodecRegistry.class.isAssignableFrom(definition.getBeanType());
     }
+}
+@Singleton
+class ReceiveDatasource {
+    @Inject
+    DataSource dataSource;
 }
