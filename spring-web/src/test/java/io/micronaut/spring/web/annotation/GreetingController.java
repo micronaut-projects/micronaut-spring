20c0435f457f3e66e7d47aec496b4be36675ece6
altro3 <alexuyt@gmail.com>
2024-01-16 19:34:04 +0700

    Add support RequestPart annotation mapping.
    Fixed #376

--- spring-web/src/test/java/io/micronaut/spring/web/annotation/GreetingController.java
+++ spring-web/src/test/java/io/micronaut/spring/web/annotation/GreetingController.java
@@ -15,6 +15,7 @@
  */
 package io.micronaut.spring.web.annotation;
 
+import io.micronaut.http.MediaType;
 import io.micronaut.validation.Validated;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -24,6 +25,9 @@ import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import jdk.jfr.ContentType;
 import reactor.core.publisher.Flux;
 
 import jakarta.validation.constraints.Pattern;
@@ -36,9 +40,9 @@ import static org.junit.Assert.assertEquals;
 @Validated
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static final String TEMPLATE = "Hello, %s!";
 
+    private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/")
     public String home(Model model) {
@@ -62,13 +66,13 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") @Pattern(regexp = "\\D+") String name) {
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+                String.format(TEMPLATE, name));
     }
 
     @PostMapping("/greeting")
     public Greeting greetingByPost(@RequestBody Greeting greeting) {
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, greeting.getContent()));
+                String.format(TEMPLATE, greeting.getContent()));
     }
 
     @DeleteMapping("/greeting")
@@ -78,11 +82,15 @@ public class GreetingController {
         return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
     }
 
-
     @RequestMapping("/greeting-status")
     @ResponseStatus(code = HttpStatus.CREATED)
     public Greeting greetingWithStatus(@RequestParam(value="name", defaultValue="World") @Pattern(regexp = "\\D+") String name) {
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+                String.format(TEMPLATE, name));
     }
+
+    @PostMapping("/multipartRequest")
+    public void multipartRequest(@RequestPart MultipartFile file) {
+    }
+
 }
