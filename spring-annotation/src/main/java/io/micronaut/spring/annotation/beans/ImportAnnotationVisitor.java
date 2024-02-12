b092bd8981a8832ed3ad63883750abf555f82a1d
Tim Yates <tim.yates@gmail.com>
2024-02-12 15:48:16 +0000

    Split method to fix critical sonar issue

--- spring-annotation/src/main/java/io/micronaut/spring/annotation/beans/ImportAnnotationVisitor.java
+++ spring-annotation/src/main/java/io/micronaut/spring/annotation/beans/ImportAnnotationVisitor.java
@@ -74,17 +74,21 @@ public final class ImportAnnotationVisitor implements TypeElementVisitor<Object,
         if (annType != null && element.hasStereotype(annType)) {
             List<AnnotationValue<? extends Annotation>> values = element.getAnnotationValuesByType(annType);
             if (!values.isEmpty()) {
-                for (AnnotationValue<?> av : values) {
-                    AnnotationClassValue<?>[] acv = av.annotationClassValues(AnnotationMetadata.VALUE_MEMBER);
-                    for (AnnotationClassValue<?> a : acv) {
-                        String className = a.getName();
-                        context.getClassElement(className).ifPresent(typeToImport -> {
-                            if (typeToImport.isPublic()) {
-                                handleImport(element, context, typeToImport);
-                            }
-                        });
+                visitValues(element, context, values);
+            }
+        }
+    }
+
+    private void visitValues(ClassElement element, VisitorContext context, List<AnnotationValue<? extends Annotation>> values) {
+        for (AnnotationValue<?> av : values) {
+            AnnotationClassValue<?>[] acv = av.annotationClassValues(AnnotationMetadata.VALUE_MEMBER);
+            for (AnnotationClassValue<?> a : acv) {
+                String className = a.getName();
+                context.getClassElement(className).ifPresent(typeToImport -> {
+                    if (typeToImport.isPublic()) {
+                        handleImport(element, context, typeToImport);
                     }
-                }
+                });
             }
         }
     }
