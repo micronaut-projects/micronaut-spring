7d1231840fb5954a0999ed5d962c51c272040b52
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-18 13:41:52 +0200

    Revert "micronaut-shared-settings = "8.0.0""
    This reverts commit 7abf16703cd6ccc72ae45ffe255823980d624866.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "8.0.0"
+    id("io.micronaut.build.shared.settings") version "8.0.0-RC1"
 }
 
 rootProject.name = "spring-parent"
